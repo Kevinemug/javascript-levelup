f06779b55c10393ecab5374de162d6db70d16d76
Kevinemug <98740834+Kevinemug@users.noreply.github.com>
2023-06-13 01:18:06 +0200

    Update persistence.js

--- CodewarsChallenges/persistence.js
+++ CodewarsChallenges/persistence.js
@@ -1,4 +1,5 @@
 // Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 const persistence = number =>{
-for(i=0;i>9 ;i++) number.split('').reduce((acc,curr)=> acc * curr)
+for(var i=0;i>9 ;i++) {number.toString.split('').reduce((acc,curr)=> acc * curr)}
+return i;
 }
