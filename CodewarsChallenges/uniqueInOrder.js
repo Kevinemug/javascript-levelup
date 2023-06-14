a98db8ec8976a87da2636d7a785a9dfd96f12d72
Kevinemug <kevineeuwamahoro@gmail.com>
2023-06-14 18:15:13 +0300

    code wars challenges

--- CodewarsChallenges/uniqueInOrder.js
+++ CodewarsChallenges/uniqueInOrder.js
@@ -1,2 +1,3 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 const  uniqueInOrder = iterable =>   [...iterable].filter((a, i) => a !== iterable[i-1])
 
